97d7f6bcd03cca3a9483c9b68bcc1f47176f5326
Alpana30 <alpana.a.chaphalkar@outlook.com>
2016-06-15 20:54:36 +0200

    Day View of Blood Sugar Graph Added.

--- www/js/controller.GraphsCtrl.js
+++ www/js/controller.GraphsCtrl.js
@@ -2,17 +2,20 @@ angular.module('starter')
 
 .controller('GraphsCtrl', function($scope, $filter, $firebaseArray, $firebaseObject){
   
+  $scope.monitorGraphs = $filter('orderBy')($scope.monitor, ['-date', 'time']);
   $scope.monitorObject = {};
   $scope.monitorObject.date = $filter('date')($scope.currentDate, "longDate");
   
   $scope.getPreviousDate = function (todaysDate) {
-     var previousDate =  new Date((new Date($scope.monitorObject.date)).getFullYear(), (new Date($scope.monitorObject.date)).getMonth(), (new Date($scope.monitorObject.date)).getDate() - 1);
+     var previousDate =  new Date((new Date(todaysDate)).getFullYear(), (new Date(todaysDate)).getMonth(), (new Date(todaysDate)).getDate() - 1);
      $scope.monitorObject.date = $filter('date')(previousDate, "longDate");
+     $scope.myJson.graphset[0].series[0].values = getBloodSugarData($scope.monitorGraphs, $scope.monitorObject.date);
   };
   
   $scope.getNextDate = function (todaysDate) {
-     var nextDate =  new Date((new Date($scope.monitorObject.date)).getFullYear(), (new Date($scope.monitorObject.date)).getMonth(), (new Date($scope.monitorObject.date)).getDate() + 1);
+     var nextDate =  new Date((new Date(todaysDate)).getFullYear(), (new Date(todaysDate)).getMonth(), (new Date(todaysDate)).getDate() + 1);
      $scope.monitorObject.date = $filter('date')(nextDate, "longDate");
+     $scope.myJson.graphset[0].series[0].values = getBloodSugarData($scope.monitorGraphs, $scope.monitorObject.date);
   };
   
   $scope.hideNextDate = function () {
@@ -24,8 +27,25 @@ angular.module('starter')
     }
   };
   
+  var getBloodSugarData = function (data, date) {
+    
+    var dataSeries = [];
+    
+    angular.forEach(data, function (record, index) {
+        var selectedDate = $filter('date')(new Date(date), "yyyy-MM-dd");
+        var recordDate = $filter('date')(new Date(record.date), "yyyy-MM-dd");
+        var recordTime = $filter('date')(new Date(record.time), "hh:mm a");
+        if (selectedDate === recordDate && record.title === "Blood Sugar") {
+          dataSeries.push([recordTime, record.value]);
+        }
+    })
+   
+   return dataSeries;
+   
+  }
+
+  //console.log(getBloodSugarData($scope.monitorGraphs, $scope.monitorObject.date));
   
-  /* 
   ZC.mobile = true;
     
     $scope.myJson = {
@@ -71,22 +91,39 @@ angular.module('starter')
       graphset:[
         {
           "type": "mixed",
+          "background-color":"#000000",
+          "alpha": 0,
           "plot": {
             "tooltip": {
-              "text": "%vt"
+              "text": "Blood Sugar: %vt "+ $scope.bloodSugarObject.unit +"<br> Time: %kt",
+              "color":"#5A00FF"
             }
           },
           "scale-x": {
             "zooming":true,
+            "line-color":"#FFFFFF",
+            "item":{
+                "color":"#FFFFFF"
+            }
           },
           "scale-y": {
-            "zooming":true
+            "zooming":true,
+            "line-color":"#FFFFFF",
+            "item":{
+                "color":"#FFFFFF"
+            }
           },
           "series": [
           {
             "type": "line",
-            "values": [],
-            "text": "Blood Sugar"
+            "values": getBloodSugarData($scope.monitorGraphs, $scope.monitorObject.date),
+            "line-color":"#FFF772",
+            "text": "Blood Sugar",
+            "marker":{
+                 "background-color":"#FFF772",
+                 "border-color":"#FFF772",
+                 "shadow":false
+            }
           },
           {
             "type": "scatter",
@@ -96,5 +133,5 @@ angular.module('starter')
         }
       ]
     };
-    */
+    
 })
